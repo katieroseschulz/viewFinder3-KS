bdf43decdfe069d1b45e70bb57a9f4b7858768dc
Apple <apple@Apples-MacBook-Pro-10.local>
2019-07-29 14:56:01 -0400

    Initial commit

---
+++ viewFinder3-KS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viewFinder3-KS
+//
+//  Created by Apple on 7/29/19.
+//  Copyright © 2019 sabyatha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
