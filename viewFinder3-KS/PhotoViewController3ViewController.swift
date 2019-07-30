dd901278bae4da26ef7dcdd520c9be37f446bbf4
Apple <apple@Apples-MacBook-Pro-10.local>
2019-07-30 14:47:43 -0400

    Complete Iteration 4

--- viewFinder3-KS/PhotoViewController3ViewController.swift
+++ viewFinder3-KS/PhotoViewController3ViewController.swift
@@ -22,22 +22,6 @@ class PhotoViewController3ViewController: UIViewController {
                     image3.image = cellPhotoImage
                 }
             }
-
-        // Do any additional setup after loading the view.
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-   
-    
-}
 }
